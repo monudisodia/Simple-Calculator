1bea55a5ee950482df281b8bc576c008558b6360
Surender Kumar <111198040+monudisodia@users.noreply.github.com>
2022-11-24 00:52:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+let ans=document.getElementById(`ans`)
+let reset=document.getElementById(`clr`)
+
+let nums=document.querySelectorAll('.num')
+
+var input=document.getElementById('input')
+
+
+input.value= '';
+for(let num of nums){
+
+    num.addEventListener('click',(e)=>{
+         str =''
+        buttonText=e.target.innerText;
+        input.value += buttonText;
+
+    })
+}
+
+reset.addEventListener('click',()=>{
+
+    input.value=''
+})
+
+ans.addEventListener('click',()=>{
+    let evaluat= eval(input.value);
+
+    input.value=evaluat
+})
